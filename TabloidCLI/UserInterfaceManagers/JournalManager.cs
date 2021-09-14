da4ca763a15e8ee4a63e713432aa668b14e6d537
IanJames2 <ianjms81@gmail.com>
2021-09-14 16:02:28 -0500

    Added Journal Manager, Repo, and model

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Management Menu");
+            Console.WriteLine(" 1) List Journals");
+            Console.WriteLine(" 0) Go Back");
+
+
+            Console.WriteLine("> ");
+            string prompt = Console.ReadLine();
+            switch (prompt)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine(journal);
+            }
+        }
+    }
+
+
+}
