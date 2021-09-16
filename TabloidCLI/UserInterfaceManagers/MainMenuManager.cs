52f4d068c02012522b2d8f6d3a68541bb39f220f
SpaceBoiLunkey <kalthenonbinarypal@gmail.com>
2021-09-16 13:13:45 -0500

    added background color and tested other modules

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
@@ -17,6 +21,7 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine(" 4) Post Management");
             Console.WriteLine(" 5) Tag Management");
             Console.WriteLine(" 6) Search by Tag");
+            Console.WriteLine(" 7) Change Background Color");
             Console.WriteLine(" 0) Exit");
 
             Console.Write("> ");
@@ -29,6 +34,32 @@ namespace TabloidCLI.UserInterfaceManagers
                 case "4": throw new NotImplementedException();
                 case "5": return new TagManager(this, CONNECTION_STRING);
                 case "6": return new SearchManager(this, CONNECTION_STRING);
+                case "7":
+                    
+                    ConsoleColor[] consoleColors
+            = (ConsoleColor[])ConsoleColor
+                  .GetValues(typeof(ConsoleColor));
+
+                    // Display the list
+                    // of available console colors
+                    Console.WriteLine("List of available "
+                                      + "Console Colors:");
+                    foreach (var color in consoleColors)
+                       Console.WriteLine(color);
+                    Console.Write("Background Color: ");
+                    string backgroundColorString = Convert.ToString(Console.ReadLine()).Trim();
+
+                    if (Enum.TryParse(backgroundColorString, ignoreCase: true, out ConsoleColor _backgroundColor))
+                        Console.BackgroundColor = _backgroundColor;
+                    
+
+                    // output to the console using the requested colors (assuming they were valid)
+
+                    Console.WriteLine("Hello World!");
+                    Console.WriteLine("Press any key to return to the menu...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    return this;
                 case "0":
                     Console.WriteLine("Good bye");
                     return null;
